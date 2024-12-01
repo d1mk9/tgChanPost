3be842445f2ed7354a00dd6684731ea74deef186
d1mk9 <d1mk9@ya.ru>
2024-12-01 18:25:41 +0300

    v1.2

---
+++ configs/configs.go
@@ -0,0 +1,47 @@
+package configs
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// Config содержит все глобальные конфигурационные параметры
+type Config struct {
+	BotToken     string
+	YandexAPIKey string
+	CatalogID    string
+	ImageAPIKey  string
+}
+
+// GlobalConfig - глобальная переменная для хранения конфигурации
+var GlobalConfig Config
+
+func LoadConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка загрузки файла .env")
+	}
+
+	GlobalConfig.BotToken = os.Getenv("TELEGRAM_APITOKEN2")
+	if GlobalConfig.BotToken == "" {
+		log.Fatal("Переменная окружения TELEGRAM_APITOKEN2 не установлена")
+	}
+
+	GlobalConfig.YandexAPIKey = os.Getenv("YANDEX_API_KEY")
+	if GlobalConfig.YandexAPIKey == "" {
+		log.Fatal("Переменная окружения YANDEX_API_KEY не установлена")
+	}
+
+	GlobalConfig.CatalogID = os.Getenv("YANDEX_CATALOG_ID")
+	if GlobalConfig.CatalogID == "" {
+		log.Fatal("Переменная окружения YANDEX_CATALOG_ID не установлена")
+	}
+
+	GlobalConfig.ImageAPIKey = os.Getenv("YANDEX_API_ART_KEY")
+	if GlobalConfig.ImageAPIKey == "" {
+		log.Fatal("Переменная окружения YANDEX_API_ART_KEY не установлена")
+	}
+
+}
--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/d1mk9/tgChanPost/configs"
 	"github.com/d1mk9/tgChanPost/internal/api"
 	"github.com/d1mk9/tgChanPost/internal/models"
 	"github.com/d1mk9/tgChanPost/internal/utils"
@@ -13,8 +14,8 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func StartBot(BotToken, yandexAPIKey, catalogID, imageAPIKey string) {
-	bot, err := tgbotapi.NewBotAPI(BotToken)
+func StartBot() {
+	bot, err := tgbotapi.NewBotAPI(configs.GlobalConfig.BotToken)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,16 +35,12 @@ func StartBot(BotToken, yandexAPIKey, catalogID, imageAPIKey string) {
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 		userQuery := update.Message.Text
-		response, err := api.GenerateMessage(yandexAPIKey, catalogID, userQuery)
+		response, err := generateResponse(userQuery)
 		if err != nil {
-			log.Printf("Ошибка генерации сообщения %d: %v", update.Message.Chat.ID, err)
-			response = models.FormattedResponse{
-				Response: "Извините, произошла ошибка при обработке вашего запроса.",
-				Status:   "error",
-			}
+			log.Printf("Ошибка генерации сообщения: %v", err)
+			continue
 		}
 
-		// Проверка на наличие ответа
 		if response.Response == "" {
 			log.Printf("Проверка наличия ответа %d", update.Message.Chat.ID)
 			continue
@@ -55,37 +52,13 @@ func StartBot(BotToken, yandexAPIKey, catalogID, imageAPIKey string) {
 			continue
 		}
 
-		// Генерация изображения на основе цитаты
-		seed := time.Now().Nanosecond() // Используем текущее время в качестве сид
-		imageFileName, err := api.GenerateArtImage(imageAPIKey, catalogID, quote, seed)
+		imageFileName, err := generateImage(quote)
 		if err != nil {
 			log.Printf("Ошибка генерации изображения %d: %v", update.Message.Chat.ID, err)
 			continue
 		}
 
-		// Проверка существования файла
-		if _, err := os.Stat(imageFileName); os.IsNotExist(err) {
-			log.Printf("Изображение не существует: %s", imageFileName)
-			continue
-		}
-
-		// Форматируем цитату для отправки
-		formattedQuote := formatQuote(quote, author)
-
-		// Открываем файл для отправки
-		file, err := os.Open(imageFileName)
-		if err != nil {
-			log.Printf("Ошибка открытия изображения: %v", err)
-			continue
-		}
-		defer file.Close()
-
-		// Отправка изображения с подписью
-		photoMsg := tgbotapi.NewPhoto(update.Message.Chat.ID, tgbotapi.FilePath(imageFileName))
-		photoMsg.ParseMode = "Markdown"
-		photoMsg.Caption = formattedQuote // Устанавливаем отформатированную цитату в качестве подписи
-
-		if _, err := bot.Send(photoMsg); err != nil {
+		if err := sendPost(bot, update.Message.Chat.ID, imageFileName, quote, author); err != nil {
 			log.Printf("Ошибка отправки изображения: %v", err)
 		}
 
@@ -104,6 +77,52 @@ func StartBot(BotToken, yandexAPIKey, catalogID, imageAPIKey string) {
 	}
 }
 
-func formatQuote(quote, author string) string {
-	return fmt.Sprintf("«%s»\n\n_%s_\n\n[%s](https://t.me/offthepages)", quote, author, "Мысли, сошедшие со страниц")
+func generateResponse(userQuery string) (models.FormattedResponse, error) {
+	response, err := api.GenerateMessage(configs.GlobalConfig.YandexAPIKey, configs.GlobalConfig.CatalogID, userQuery)
+	if err != nil {
+		return models.FormattedResponse{}, err
+	}
+	return response, nil
+}
+
+func generateImage(quote string) (string, error) {
+	// Генерация изображения на основе цитаты
+	seed := time.Now().Nanosecond() // Используем текущее время в качестве сид
+	imageFileName, err := api.GenerateArtImage(configs.GlobalConfig.ImageAPIKey, configs.GlobalConfig.CatalogID, quote, seed)
+	if err != nil {
+		return "", err
+	}
+
+	// Проверка существования файла
+	if _, err := os.Stat(imageFileName); os.IsNotExist(err) {
+		return "", fmt.Errorf("изображение не существует: %s", imageFileName)
+	}
+
+	return imageFileName, nil
+}
+
+func sendPost(bot *tgbotapi.BotAPI, chatID int64, imageFileName, quote, author string) error {
+	// Форматируем цитату для отправки
+	formattedQuote := fmt.Sprintf("«%s»\n\n_%s_\n\n[%s](https://t.me/offthepages)", quote, author, "Мысли, сошедшие со страниц")
+	// Открываем файл для отправки
+	file, err := os.Open(imageFileName)
+	if err != nil {
+		return fmt.Errorf("ошибка открытия изображения: %v", err)
+	}
+	defer file.Close()
+
+	// Отправка изображения с подписью
+	photoMsg := tgbotapi.NewPhoto(chatID, tgbotapi.FilePath(imageFileName))
+	photoMsg.ParseMode = "Markdown"
+	photoMsg.Caption = formattedQuote // Устанавливаем отформатированную цитату в качестве подписи
+
+	if _, err := bot.Send(photoMsg); err != nil {
+		return fmt.Errorf("ошибка отправки изображения: %v", err)
+	}
+
+	msgtoch := tgbotapi.NewPhotoToChannel("@offthepages", photoMsg.File)
+	msgtoch.ParseMode = "Markdown"
+	msgtoch.Caption = formattedQuote // Устанавливаем отформатированную цитату в качестве подписи
+	bot.Send(msgtoch)
+	return nil
 }
