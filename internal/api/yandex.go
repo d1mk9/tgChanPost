84b0bca1089679dde4552960341650f9cab1b093
d1mk9 <d1mk9@ya.ru>
2024-12-10 23:11:39 +0300

    v1.3.1

--- internal/api/yandex.go
+++ internal/api/yandex.go
@@ -197,7 +197,7 @@ func GenerateArtImage(apiKey, catalogID, prompt string, seed int) (string, error
 			}
 
 			dataFile := time.Now().Format("2006-01-02_15-04-05")
-			imageFileName := dataFile + ".jpeg"
+			imageFileName := "/mount/dir/" + dataFile + ".jpeg"
 			imageFile, err := os.Create(imageFileName)
 			if err != nil {
 				return "", err
